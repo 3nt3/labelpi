59c3eba9666ca757e81e06f6d84c5140d1b0656d
Nia Schlegel <nia@bizfactory.tech>
2025-02-02 14:54:11 +0100

    test test
    Signed-off-by: Nia Schlegel <nia@bizfactory.tech>

--- printing.py
+++ printing.py
@@ -1,6 +1,4 @@
-from brother_ql.raster import BrotherQLRaster
-from brother_ql.conversion import convert
-from brother_ql.backends.helpers import send
+from brother_label import BrotherLabel
 import typst
 import tempfile
 
@@ -11,7 +9,7 @@ def print_label(text, preformatted=False):
 
     label_size = '62'
 
-    qlr = BrotherQLRaster(printer_model)
+    brother_label = BrotherLabel(device=printer_model, target=printer_address, backend=printer_backend)
 
     tmp_png = tempfile.NamedTemporaryFile(mode="w+")
 
@@ -27,11 +25,7 @@ def print_label(text, preformatted=False):
 
         typst.compile(tmp.name, output=tmp_png.name, format="png", ppi=600)
 
-    # Convert the image to the correct format and print
-    convert(qlr, [tmp_png.name], label_size, cut=True, red=True, dither=True)
-
-    # Send the label to the printer
-    send(instructions=qlr.data, printer_identifier=printer_address, backend_identifier=printer_backend)
+    brother_label.print(label_size, [tmp_png.name], cut=True, red=True, dither=True)
 
     print("Label sent to the printer.")
 
@@ -43,12 +37,8 @@ def print_image(image):
 
     label_size = '62'
 
-    qlr = BrotherQLRaster(printer_model)
+    brother_label = BrotherLabel(device=printer_model, target=printer_address, backend=printer_backend)
 
-    # Convert the image to the correct format and print
-    convert(qlr, [image], label_size, cut=True, red=True, dither=True)
-
-    # Send the label to the printer
-    send(instructions=qlr.data, printer_identifier=printer_address, backend_identifier=printer_backend)
+    brother_label.print(label_size, [image])
 
     print("Label sent to the printer.")
